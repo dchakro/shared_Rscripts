d91655c10ac21428dc0ed6514fd12ba6484cfcd3
dchakro <35454738+dchakro@users.noreply.github.com>
2021-05-06 11:59:13 +0300

    add instructions

--- PCR_formula.R
+++ PCR_formula.R
@@ -1,6 +1,17 @@
+#### DESCRIPTION:
 # These functions use the PCR equation to calculate
 # the number of PCR products after N cycles starting
 # from x molecules of the template.
+
+
+#### HOW TO USE:
+# source('https://github.com/dchakro/shared_Rscripts/raw/master/PCR_formula.R')
+# prettyNum(PCRamp(1,30),big.mark = " ")
+# 
+# print(paste("Cycle",1:30,"=",
+#             prettyNum(PCRamp.steps(1,30),
+#                       big.mark = " ")),
+#       width = 40)
 
 PCRamp = function(x, n){
   # calculates number of amplicons starting from x initial DNA molecules in n cycles.
